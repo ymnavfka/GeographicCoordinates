547fc63e3ac3b21fc8d930a0f23c81bd6deb0f88
Nikita Morozov <ymnavfka@mail.ru>
2024-12-02 23:04:13 +0300

    Changed London to St.Petersburg

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 int main() {
     try {
         GeoPoint point1(55.7558, 37.6173); // Москва
-        GeoPoint point2(51.5074, -0.1278); // Лондон
+        GeoPoint point2(59.94, 30.32); // Питер
 
         std::cout << point1 << "\n";
         std::cout << "Latitude Hemisphere: " << point1.getLatitudeHemisphere() << "\n";
@@ -15,10 +15,10 @@ int main() {
         std::cout << "Longitude Hemisphere: " << point2.getLongitudeHemisphere() << "\n";
 
         double distance = point1.calculateDistance(point2);
-        std::cout << "\nDistance between Moscow and London: " << distance << " km\n";
+        std::cout << "\nDistance between Moscow and St.Petersburg: " << distance << " km\n";
 
         std::cout << "Time offset for Moscow: " << point1.getTimeOffset() << " minutes\n";
-        std::cout << "Time offset for London: " << point2.getTimeOffset() << " minutes\n";
+        std::cout << "Time offset for St.Petersburg: " << point2.getTimeOffset() << " minutes\n";
 
     } catch (const std::exception& e) {
         std::cerr << "Error: " << e.what() << "\n";
